55761da92f3d9db953a7dbe09a92ade272abef31
KamyaPA <peteraksel@live.dk>
2023-10-26 15:29:09 +0200

    Simplified client

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,11 @@ pub mod services {
     tonic::include_proto!("ecdar_proto_buf");
 }
 
-pub use services::ecdar_backend_client::EcdarBackendClient as Client;
-pub use services::ecdar_backend_server::EcdarBackendServer as Server;
+use services::ecdar_backend_client::EcdarBackendClient;
+pub type Client = EcdarBackendClient<Channel>;
+use services::ecdar_backend_server::EcdarBackendServer;
+pub type Server = EcdarBackendServer<Channel>;
 pub use services::*;
+use tonic::transport::Channel;
 
 
