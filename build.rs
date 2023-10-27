562eba5d9679be8121b6f96be5bec198824d673b
KamyaPA <peteraksel@live.dk>
2023-10-27 14:00:33 +0200

    updated buildscript

--- build.rs
+++ build.rs
@@ -1,4 +1,14 @@
-fn main() {
-    tonic_build::compile_protos("Ecdar-ProtoBuf/services.proto").unwrap();
-    println!("cargo:rerun-if-changed=Ecdar-ProtoBuf");
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>>{
+    tonic_build::configure()
+        .build_client(true)
+        .build_server(false)
+        .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]")
+        .emit_rerun_if_changed(true)
+        .compile(&["Ecdar-ProtoBuf/services.proto"], &["Ecdar-ProtoBuf"])?;
+
+    println!("cargo:rerun-if-changed=build.rs");
+
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
-
 pub mod services {
     tonic::include_proto!("ecdar_proto_buf");
 }
 
+use services::ecdar_backend_client::EcdarBackendClient;
+
+use tonic::transport::Channel;
+pub type Client = EcdarBackendClient<Channel>;
 
 
