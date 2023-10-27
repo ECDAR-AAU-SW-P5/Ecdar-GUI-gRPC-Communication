39ad93e43208a43de073f5f2a68e9aec70caecb4
KamyaPA <peteraksel@live.dk>
2023-10-27 12:42:42 +0200

    compils protobuffers

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,5 @@ pub mod services {
     tonic::include_proto!("ecdar_proto_buf");
 }
 
-use services::ecdar_backend_client::EcdarBackendClient;
-pub type Client = EcdarBackendClient<Channel>;
-use services::ecdar_backend_server::EcdarBackendServer;
-pub type Server = EcdarBackendServer<Channel>;
-pub use services::*;
-use tonic::transport::Channel;
 
 
