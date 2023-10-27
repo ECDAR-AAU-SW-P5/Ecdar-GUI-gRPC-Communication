ffe9a164c6b8148b719fff9d55292171dedecfad
KamyaPA <peteraksel@live.dk>
2023-10-27 14:25:11 +0200

    rename all json to camelCase

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ fn main() -> Result<(), Box<dyn Error>>{
     tonic_build::configure()
         .build_client(true)
         .build_server(false)
-        .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]")
+        .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]\n#[serde(rename_all=\"camelCase\")]")
         .emit_rerun_if_changed(true)
         .compile(&["Ecdar-ProtoBuf/services.proto"], &["Ecdar-ProtoBuf"])?;
 
