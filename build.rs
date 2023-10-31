aefbac9452cc5bd900ea07af459877e3514e3cc5
KamyaPA <peteraksel@live.dk>
2023-10-31 10:36:19 +0100

    setup as lib

--- build.rs
+++ build.rs
@@ -1,10 +1,21 @@
 use std::error::Error;
 
-fn main() -> Result<(), Box<dyn Error>>{
-    tonic_build::configure()
-        .build_client(true)
-        .build_server(false)
-        .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]\n#[serde(rename_all=\"camelCase\")]")
+fn main() -> Result<(), Box<dyn Error>> {
+    let build = tonic_build::configure();
+
+    #[cfg(feature = "client")]
+    let build = build.build_client(true);
+
+    #[cfg(feature = "server")]
+    let build = build.build_server(true);
+
+    #[cfg(feature = "serde")]
+    let build = build.type_attribute(
+        ".",
+        "#[derive(serde::Deserialize, serde::Serialize)]\n" + "#[serde(rename_all=\"camelCase\")]",
+    );
+
+    build
         .emit_rerun_if_changed(true)
         .compile(&["Ecdar-ProtoBuf/services.proto"], &["Ecdar-ProtoBuf"])?;
 
--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,5 @@ pub mod services {
     tonic::include_proto!("ecdar_proto_buf");
 }
 
-use services::ecdar_backend_client::EcdarBackendClient;
-
-use tonic::transport::Channel;
-pub type Client = EcdarBackendClient<Channel>;
-
-
+#[cfg(feature = "client")]
+pub type Client = services::ecdar_backend_client::EcdarBackendClient<tonic::transport::Channel>;
