20f72a774b52eb8ee44caebd98af77dd8a93eb4f
KamyaPA <peteraksel@live.dk>
2023-11-02 10:48:40 +0100

    More verbose features

--- build.rs
+++ build.rs
@@ -4,9 +4,27 @@ fn main() -> Result<(), Box<dyn Error>> {
     let build = tonic_build::configure();
 
     #[cfg(feature = "client")]
+    #[cfg(not(feature = "server"))]
+    let build = build.build_client(true);
+
+    #[cfg(feature = "client")]
+    #[cfg(not(feature = "server"))]
+    let build = build.build_server(false);
+
+    #[cfg(feature = "server")]
+    #[cfg(not(feature = "client"))]
+    let build = build.build_server(true);
+
+    #[cfg(feature = "server")]
+    #[cfg(not(feature = "client"))]
+    let build = build.build_client(false);
+
+    #[cfg(feature = "server")]
+    #[cfg(feature = "client")]
     let build = build.build_client(true);
 
     #[cfg(feature = "server")]
+    #[cfg(feature = "client")]
     let build = build.build_server(true);
 
     #[cfg(feature = "serde")]
--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,3 @@ pub mod services {
     tonic::include_proto!("ecdar_proto_buf");
 }
 
-#[cfg(feature = "client")]
-pub type Client = services::ecdar_backend_client::EcdarBackendClient<tonic::transport::Channel>;
