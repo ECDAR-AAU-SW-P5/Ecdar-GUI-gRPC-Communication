70335d5a95971260ccb66249c1453db2ef04fdfc
KamyaPA <peteraksel@live.dk>
2023-11-03 09:08:53 +0100

    Fixed features

--- build.rs
+++ build.rs
@@ -3,29 +3,11 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
     let build = tonic_build::configure();
 
-    #[cfg(feature = "client")]
-    #[cfg(not(feature = "server"))]
-    let build = build.build_client(true);
-
-    #[cfg(feature = "client")]
     #[cfg(not(feature = "server"))]
     let build = build.build_server(false);
 
-    #[cfg(feature = "server")]
-    #[cfg(not(feature = "client"))]
-    let build = build.build_server(true);
-
-    #[cfg(feature = "server")]
     #[cfg(not(feature = "client"))]
     let build = build.build_client(false);
-
-    #[cfg(feature = "server")]
-    #[cfg(feature = "client")]
-    let build = build.build_client(true);
-
-    #[cfg(feature = "server")]
-    #[cfg(feature = "client")]
-    let build = build.build_server(true);
 
     #[cfg(feature = "serde")]
     let build = build.type_attribute(
